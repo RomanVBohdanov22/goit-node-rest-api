336db76f2ab67b1887679decaa47456f2b2cb86b
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-16 15:34:09 +0200

    bored)

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,14 +2,39 @@
 const contactsService = require("../services/contactsServices");
 import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
 
 //node app.js
