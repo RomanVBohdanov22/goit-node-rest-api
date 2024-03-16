295f7862ebe983c201ca3b04f7f4fd559d547a40
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-16 16:01:04 +0200

    bired that

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,36 +2,62 @@
 const contactsService = require("../services/contactsServices");
 import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res, next) => {
+export const getAllContacts = async (req, res, next) => {
   try {
+    const result = await contactsServices.listContacts();
+    res.json(result);
   } catch (error) {
     next(error);
   }
 };
 
-export const getOneContact = (req, res, next) => {
+export const getOneContact = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const result = await contactsServices.getContactById(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
   } catch (error) {
     next(error);
   }
 };
 
-export const deleteContact = (req, res, next) => {
+export const deleteContact = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const result = await contactsServices.removeContact(id);
+    if (result === null) {
+      throw HttpError(404, "Contact not found");
+    }
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 };
 
-export const createContact = (req, res, next) => {
+export const createContact = async (req, res, next) => {
   try {
+    const { name, email, phone } = req.body;
+    const result = await contactsServices.addContact(name, email, phone);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 };
 
-export const updateContact = (req, res, next) => {
+export const updateContact = async (req, res, next) => {
   try {
+    const { id } = req.params;
+    const result = await contactsServices.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Contact not found");
+    }
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   }
