83911449050f24f35fc76942bc986c1a5ac0e570
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-25 00:19:05 +0200

    mongoose begin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
