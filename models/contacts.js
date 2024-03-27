9daa0f0101b03fe5fdcfd6b35607fe911ac3d5bf
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-27 21:28:25 +0200

    ne skladeno!

--- models/contacts.js
+++ models/contacts.js
@@ -6,12 +6,42 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+  // favorite: Joi.boolean().required(),
+});
+
+export const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
-})
-  .min(1)
-  .message("Body must have at least one field");
+});
+//.min(1)
+//.message("Body must have at least one field");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+export const Contact = model("contact", contactSchema);
